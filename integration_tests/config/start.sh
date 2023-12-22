ed8563928d1c2c7ae6f73053ea1d645d6e2ac0a6
Darren Kelly <darren.kelly@vechain.org>
2023-12-22 10:05:12 +0000

    update start script to get IP address

--- integration_tests/config/start.sh
+++ integration_tests/config/start.sh
@@ -9,4 +9,8 @@ echo "Starting node with master key address $MASTER_KEY_ADDRESS"
 
 cp /node/keys/$MASTER_KEY_ADDRESS/master.key /tmp
 
-thor --config-dir=/tmp --network /node/config/genesis.json --bootnode enode://e32e5960781ce0b43d8c2952eeea4b95e286b1bb5f8c1f0c9f09983ba7141d2fdd7dfbec798aefb30dcd8c3b9b7cda8e9a94396a0192bfa54ab285c2cec515ab@10.5.0.2:55555
+BOOTNODE_IP=$(ping -c 1 thor-disco | awk -F'[()]' '/PING/{print $2}')
+
+echo $BOOTNODE_IP
+
+thor --config-dir=/tmp --network /node/config/genesis.json --bootnode "enode://e32e5960781ce0b43d8c2952eeea4b95e286b1bb5f8c1f0c9f09983ba7141d2fdd7dfbec798aefb30dcd8c3b9b7cda8e9a94396a0192bfa54ab285c2cec515ab@$BOOTNODE_IP:55555"
